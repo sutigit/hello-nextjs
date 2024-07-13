74690cb6b53d811f880d7619567dc2cfc16585bc
sutigit <panyanilben@gmail.com>
2024-07-13 11:56:04 +0300

    chapter 3

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Poppins, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const poppins = Poppins({ 
+    weight: ['300', '500', '800'],
+    subsets: ['latin'] 
+});
+
+export const lusitana = Lusitana({ 
+    weight: ['400', '700'],
+    subsets: ['latin'] 
+});
